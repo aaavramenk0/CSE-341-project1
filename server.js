49477f13cfee5684e814fd900bfd852cabe74f30
Aleksandr <alex.avramenko2004@gmail.com>
2023-06-18 16:07:42 -0400

    Assignment 4

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
     next();
   })
   .use('/', require('./routes'));
